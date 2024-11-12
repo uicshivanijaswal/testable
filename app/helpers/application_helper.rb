e8eb3451bd5422256554b48650f5c868ac6e36ff
Shivani <uic.shivanijaswal@gmail.com>
2024-11-12 11:18:29 +0000

    add posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
+# This is the top level comment for application_helper
 module ApplicationHelper
 end
---
+++ app/helpers/posts_helper.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# This is the top level comment for posts_helper
+module PostsHelper
+end
